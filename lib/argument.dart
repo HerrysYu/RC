c11c0fc58676947ca1791e0257252cdcc304854a
HerrysYu <exyuq2012@gmail.com>
2023-05-27 23:14:04 +0800

    modified

--- lib/argument.dart
+++ lib/argument.dart
@@ -4,7 +4,8 @@ class Arguments {
   static String message = "";
   static int istapped = 0;
   static int counter = 0;
-  static String SeverAddress = "ws://192.168.1.9:8765";
+  static String SeverAddress = "ws://139.180.146.42:8765";
+  //static String SeverAddress = "ws://192.168.124.7:8765";
   static String? CurrentWord;
   static String database_path = 'save.db';
   static bool isDatasetExits = false;
