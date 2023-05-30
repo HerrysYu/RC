a23ac56cd66807e3ca5956c992b106dac73969c4
HerrysYu <exyuq2012@gmail.com>
2023-05-30 23:36:56 +0800

    VPM

--- lib/Voabulary.dart
+++ lib/Voabulary.dart
@@ -9,15 +9,65 @@ import 'dart:async';
 import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:provider/provider.dart';
 class VocabularyState extends ChangeNotifier{
   List list =sqlHelper.ReadOut();
+  ListRefresh()async{
+  list=sqlHelper.ReadOut();
+  notifyListeners();
+  }
 }
-class vocabulary extends StatelessWidget{
+final VocabularyState vocabularyState=VocabularyState();
+class VList extends StatelessWidget{
+  
   @override
   Widget build(BuildContext context) {
-    var VocabularyState=context.watch<VocabularyState>;
+    var state=context.watch<VocabularyState>();
+    List list=state.list;
+    return Column(
+      children: [
+        //for(var o in list)
+          //Text(o)
+        Text("Text")
+      ]
+    );
     // TODO: implement build
-    return ChangeNotifierProvider(child: MaterialApp(home: Scaffold(body: Column(children: [],)),));
     throw UnimplementedError();
   }
 }
+
+class vocabulary extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ChangeNotifierProvider(
+      create: (_) => VocabularyState(),
+     child: Scaffold(body: Hero(tag:'TP',child: Center(child: VList(),)),),
+);
+  }
+  }
+
+class ScroolView extends StatelessWidget {
+  const ScroolView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(slivers: [Bar(),VList()],);
+  }
+}
+  class Bar extends StatelessWidget{
+    @override
+  Widget build(BuildContext context) {
+    var State=context.watch<VocabularyState>();
+    return SliverAppBar(
+      flexibleSpace: const FlexibleSpaceBar(
+    title: Text('AppBar'),
+  ),
+  onStretchTrigger: State.ListRefresh(),
+);
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  }
